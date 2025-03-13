d344d6c6e8a141fe6b80d71ed08017729a81b0c1
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-12 22:45:37 -0300

    Codando o exec 05

---
+++ src/Exec_05/Bilhete.java
@@ -0,0 +1,48 @@
+package Exec_05;
+
+import java.util.Random;
+
+public class Bilhete {
+    static final double TARIFABASE = 5.20;
+    int Id;
+    double saldo;
+    Usuario usuario;
+
+    public Bilhete(Usuario usuario){
+
+        Id = gerarNumero();
+        this.usuario = usuario;
+
+    }
+
+    private int gerarNumero() {
+        Random random = new Random();
+        return random.nextInt(1000,10000);
+    }
+
+    // metodo para carregar o blihete
+    public void carregarBilhete (double recarga) {
+        this.saldo += recarga;
+    }
+
+    // metodo para consultar o saldo
+    public double consultarSaldo () {
+        return this.saldo;
+    }
+
+    // metodo para passar na catraca
+    public void passarCatraca() {
+        double debito;
+        if (usuario.tipoTarifa.equalsIgnoreCase("tarifa")){
+            debito = TARIFABASE;
+        }
+        else {
+            debito = TARIFABASE/2;
+        }
+
+        if (saldo >= debito) {
+
+        }
+    }
+
+}
---
+++ src/Exec_05/Usuario.java
@@ -0,0 +1,13 @@
+package Exec_05;
+
+public class Usuario {
+    String nome, tipoTarifa;
+    long cpf;
+
+    public Usuario(String nome, long cpf, String tipoTarifa) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.tipoTarifa = tipoTarifa;
+    }
+
+}
